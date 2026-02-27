f5449ab8f667ce1eba67a1b6c4dd2321289da41b
Megh Patel <meghjptl2008@gmail.com>
2026-02-26 22:42:25 -0700

    Fix 502 error final

--- elearning-project/app.py
+++ elearning-project/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "eLearning Project is Live!"
+    return "Cloud Based E-Learning Project is Live!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
